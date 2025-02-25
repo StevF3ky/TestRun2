cefd2cff4c2414bab0e7ce80bc220b704cde12b8
SQFr3aky <fr3aky009@gmail.com>
2025-02-25 21:08:01 +0700

    penambahan fungsion pengurangan

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def penambahan (a,b):
 	total = a + b
 	return (total)
 
+def pengurangan (a,b):
+	total = a - b
+	return (total)
+
 def main ():
 	print (penambahan(5,10))
+	print (pengurangan(10,5))
 
 main ()
